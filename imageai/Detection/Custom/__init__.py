90ffa785e50d8b316787f3736e5d0d1cc2125ad9
apavlides <alex@pyrratech.com>
2023-07-03 12:45:42 +0000

    feat: add f1 array to return in __init__.py

--- imageai/Detection/Custom/__init__.py
+++ imageai/Detection/Custom/__init__.py
@@ -331,7 +331,7 @@ class DetectionModelTrainer:
         elapsed_time = time.time() - since
         print(f"Training completed in {elapsed_time//60:.0f}m {elapsed_time % 60:.0f}s")
         torch.cuda.empty_cache()
-        return mp_array, mr_array, map50_array, map50_95_array, ap_per_class_array
+        return mp_array, mr_array, map50_array, map50_95_array, f1_array, ap_per_class_array
 
 
 class CustomObjectDetection:
